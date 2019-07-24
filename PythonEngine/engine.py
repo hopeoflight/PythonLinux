f6d9046c262acee4b1898a2ae9780efaa1d832ed
Baizhiquan <18565635063@163.com>
2019-07-24 16:38:55 +0800

    add commandline parse

--- PythonEngine/engine.py
+++ PythonEngine/engine.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 import json
+import os
+import argparse
 
 
 # 引擎脚本,用于调用不同扫描模块或者直接调用正则处理漏洞文件
@@ -47,16 +49,62 @@ class Engine:
         return json.dumps(Engine.__Vul.get_index(vul_id, style), ensure_ascii=False)
 
     @staticmethod
-    def get_id(style):
+    def get_id(style=None):
         return json.dumps(Engine.__Vul.get_id(style), ensure_ascii=False)
 
     @staticmethod
-    def get_type(vul_id):
+    def get_type(vul_id=None):
         return json.dumps(Engine.__Vul.get_type(vul_id), ensure_ascii=False)
 
     @staticmethod
     def get_content(vul_id=None, style=None):
         return json.dumps(Engine.__Vul.get_content(vul_id, style), ensure_ascii=False)
+
+    @staticmethod
+    def save_vul(v_list):
+        for v in v_list:
+            Engine.insert(v["nneId"], v["type"], 0, "", json.dumps(v))
+
+    @staticmethod
+    def save_vul_file(file):
+        if not os.path.exists(file):
+            print("cannot find file %s" % file)
+            return
+
+        with open(file, 'rb') as f:
+            content = f.read()
+        if len(content) == 0:
+            return
+
+        Engine.save_vul(eval(content))
+
+
+def main(args):
+    if args.arg_type == "module":
+        if not args.py_module:
+            print("please input module name!")
+        else:
+            Engine.run_module(args.py_module)
+    elif args.arg_type == "append":
+        if not args.flaw_id or not args.flaw_type or not args.flaw_content:
+            print("cannot get flaw id or flow type or flaw content,please input!")
+        else:
+            Engine.insert(args.flaw_id, int(args.flaw_type), int(args.secret_type), args.secret_key, args.flaw_content)
+    elif args.arg_type == "append_file":
+        if not args.flaw_file:
+            print("please input flaw file path!")
+        else:
+            Engine.save_vul_file(args.flaw_file)
+    elif args.arg_type == "index":
+        print(Engine.get_index(args.flaw_id, int(args.flaw_type) if args.flaw_type else None))
+    elif args.arg_type == "id":
+        print(Engine.get_id(int(args.flaw_type) if args.flaw_type else None))
+    elif args.arg_type == "type":
+        print(Engine.get_type(args.flaw_id))
+    elif args.arg_type == "content":
+        print(Engine.get_content(args.flaw_id, int(args.flaw_type) if args.flaw_type else None))
+    else:
+        print("invalid arg type")
 
 
 if __name__ == "__main__":
@@ -64,5 +112,19 @@ if __name__ == "__main__":
     # Engine.run_module("load_test")
     # Engine.run_module("module1")
     # Engine.run_module("module2")
-    print(Engine.get_content())
-    pass
+    # print(Engine.get_content(vul_id="test1"))
+    # Engine.save_vul_file("vulnerable")
+
+    parser = argparse.ArgumentParser(usage="usage tip.", description="help info.")
+    parser.add_argument("-t",
+                        required=True,
+                        choices=['module', 'append', 'append_file', 'index', 'id', 'type', 'content'],
+                        dest="arg_type")
+    parser.add_argument("-m", help="input module name", dest="py_module")
+    parser.add_argument("-i", help="input flaw id", dest="flaw_id")
+    parser.add_argument("-s", help="input flaw type", dest="flaw_type")
+    parser.add_argument("-st", default="0", help="input secret type", dest="secret_type")
+    parser.add_argument("-sk", default="", help="input secret key", dest="secret_key")
+    parser.add_argument("-c", help="input flaw content", dest="flaw_content")
+    parser.add_argument("-f", help="input file path", dest="flaw_file")
+    main(parser.parse_args())
